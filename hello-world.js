fc8f27f2c476cd9df5ba7fd22f611032d9da1b7a
Gabriel Barcelos <gabriel.barcelos.araujo@outlook.com>
2023-08-29 18:32:25 -0300

    hello world

--- hello-world.js
+++ hello-world.js
@@ -1,10 +1,1 @@
-//console.log("Hello world!")
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-  readline.question(`What's your name?`, name => {
-    console.log(`Hi ${name}!`)
-    readline.close()
-})
+console.log("Hello world!")
