23355b0df6d486d67e66343db7f9729bc00d06fb
Gabriel Barcelos <gabriel.barcelos.araujo@outlook.com>
2023-08-31 18:28:18 -0300

    par-impar

---
+++ par-impar.mjs
@@ -0,0 +1,17 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const rl = readline.createInterface({ input, output })
+
+function checkoddeven(number) {
+    if (number % 2 === 0) {
+        return "par"
+    } else {
+        return "ímpar"
+    }
+}
+const number = parseFloat(await rl.question("enter the number: \n"))
+const result = checkoddeven(number)
+console.log(`the number ${number} is ${result}.`)
+
+rl.close()
+
