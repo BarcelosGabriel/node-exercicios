2f4bafb35424c93555c63ed95d31b9189129a687
Gabriel Barcelos <gabriel.barcelos.araujo@outlook.com>
2023-09-08 16:58:55 -0300

    ajuste codigo fibonacci.mjs

--- src/fibonacci.mjs
+++ src/fibonacci.mjs
@@ -1,3 +1,7 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const rl = readline.createInterface({ input, output })
+
 function fibonacci(n) {
   const fibNumbers = [0, 1];
 
@@ -9,5 +13,7 @@ function fibonacci(n) {
   return fibNumbers.slice(0, n);
 }
 
-const trintaPrimeirosFibonacci = fibonacci(30);
-console.log(trintaPrimeirosFibonacci);
+const Fib= await rl.question("add the number: \n")
+console.log(fibonacci(Fib));
+
+rl.close()
